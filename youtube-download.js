a2ab1abcd5a57e8f15d7a1b2865d2617c4abc156
Om Shejul <contact@omshejul.com>
2024-06-05 14:26:39 +0530

    fix repo

---
+++ youtube-download.js
@@ -0,0 +1,49 @@
+module.exports = function(RED){
+    "use strict";
+
+    function YoutubeDownload(n){
+        RED.nodes.createNode(this, n);
+        this.url = n.url;
+        this.urlType = n.urlType;
+        this.path = n.path;
+        this.pathType = n.pathType;
+        var Ytdl = require('ytdl-core');
+        var Fs = require('fs');
+        var Progress = require('progress-stream');
+        var progressStream = Progress({time:500});
+
+        this.status({});
+        var node = this;
+
+        this.on('input', function(msg){
+            node.status({fill:"blue", shape:"ring", text:"Starting"});
+            var path_ = RED.util.evaluateNodeProperty(node.path, node.pathType, node, msg);
+            var url_ = RED.util.evaluateNodeProperty(node.url, node.urlType, node, msg);
+
+
+            progressStream.on('progress', function(progress) {
+                node.status({fill:"blue", shape:"ring", text:progress.percentage.toFixed(2) + "%"});
+            });
+
+            var ytdl = Ytdl(url_, { filter: function(format) { return format.container === 'mp4'; } });
+
+            ytdl.on('response', function(response) {
+                progressStream.setLength( response.headers["content-length"] );
+            });
+            ytdl.pipe(progressStream).pipe(Fs.createWriteStream(path_))
+            .on('finish', function(){
+                node.status({fill:"blue", shape:"dot", text:"Done"});
+                node.send(msg);
+            })
+            node.ytdl = ytdl;
+        });
+
+        this.on('close', function(){
+            if(node.ytdl && node.ytdl.destroy)
+                node.ytdl.destroy();
+            if(progressStream && progressStream.destroy)
+                progressStream.destroy();
+        });
+    }
+    RED.nodes.registerType("youtube-download", YoutubeDownload);
+}
---
+++ youtube-upload.js
@@ -0,0 +1,103 @@
+module.exports = function(RED){
+    "use strict";
+
+    function YoutubeUpload(n){
+        RED.nodes.createNode(this, n);
+        this.google = RED.nodes.getNode(n.google);
+        this.privacyStatus = n.privacyStatus;
+        this.mediapath = n.mediapath;
+        this.mediapathType = n.mediapathType;
+        this.title = n.title;
+        this.titleType = n.titleType;
+        this.description = n.description;
+        this.descriptionType = n.descriptionType;
+        var Youtube = require('youtube-api');
+        var Fs = require('fs');
+        var Path = require('path');
+        var Progress = require('progress-stream');
+
+
+        this.status({});
+        var node = this;
+
+        this.on('input', function(msg){
+            function processInput(){
+                node.status({fill:"blue", shape:"ring", text:"uploading"});
+                var progressStream = Progress({time:500});
+                Youtube.authenticate({
+                    type: "oauth"
+                    , token: node.google.credentials.accessToken
+                });
+                progressStream.on('progress', function(progress) {
+                    node.status({fill:"blue", shape:"ring", text:progress.percentage.toFixed(2) + "%"});
+                });
+
+                var videoPath = Path.resolve(RED.util.evaluateNodeProperty(node.mediapath, node.mediapathType, node, msg));
+                var title_ = RED.util.evaluateNodeProperty(node.title, node.titleType, node, msg);
+                var description_ = RED.util.evaluateNodeProperty(node.description, node.descriptionType, node, msg)
+
+                var stat = Fs.statSync(videoPath);
+                progressStream.setLength(stat.size);
+
+                node.uploadStream = Fs.createReadStream(videoPath).pipe(progressStream);
+
+                node.youtubeInsertReq = Youtube.videos.insert({
+                    resource: {
+                        snippet: {
+                            title: title_,
+                            description: description_,
+                        },
+
+                        status: {
+                            privacyStatus: node.privacyStatus
+                        }
+                    },
+
+                    part: "snippet,status",
+
+                    // Create the readable stream to upload the video
+                    media: {
+                        body: node.uploadStream
+                    }
+                }, function(err, data){
+                    if(err){
+                        node.status({fill:"red", shape:"dot", text:"Error " + err});
+                        node.error(err);
+                    }else{
+                        node.status({});
+                        msg.payload = data;
+                        node.send(msg);
+                    }
+                })
+            }
+
+            var curTime = new Date().getTime() / 1000;
+            if(node.google && node.google.credentials){
+                var expireTime = node.google.credentials.expireTime;
+                var expired = (curTime > expireTime);
+                if(expired){
+                    console.log('expired token');
+                    node.google.refreshToken(function(){
+                        console.log("refreshed");
+                        processInput();
+                    });
+                } else{
+                    processInput();
+                }
+            }
+        });
+
+        this.on('close', function(){
+            //End upload
+            if(node.uploadStream && node.uploadStream.destroy)
+                node.uploadStream.destroy();
+            if(node.youtubeInsertReq){
+                if(node.youtubeInsertReq.destroy)
+                    node.youtubeInsertReq.destroy();
+                delete node.youtubeInsertReq;
+            }
+
+        });
+    }
+    RED.nodes.registerType("youtube-upload", YoutubeUpload);
+}
